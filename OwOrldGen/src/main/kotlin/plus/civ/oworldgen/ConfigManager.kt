bb4bab44463b8367f4f9ed3d53339750616f902b
SOERXPSO <soerxpso@gmail.com>
2020-06-30 16:26:08 -0400

    i forget. oworldgen stuff

--- OwOrldGen/src/main/kotlin/plus/civ/oworldgen/ConfigManager.kt
+++ OwOrldGen/src/main/kotlin/plus/civ/oworldgen/ConfigManager.kt
@@ -7,35 +7,31 @@ import org.bukkit.configuration.ConfigurationSection
 import java.awt.Color
 
 class ConfigManager(conf: ConfigurationSection) {
-    private val biomeColors = HashMap<Color, Biome>()
-	private val biomeTopblocks = HashMap<Biome, Material>()
+	private class BiomeInfo(val topblock: Material, val fineCoeff: Double, val hillCoeff: Double, val broadCoeff: Double) {}
+	private val biomeInfos = HashMap<Biome, BiomeInfo>()
     init {
         val biomesSection = conf.getConfigurationSection("biomes")!!
         for (biomeName in biomesSection.getKeys(false)) {
             val biomeSection = biomesSection.getConfigurationSection(biomeName)!!
-            val colorHexString = biomeSection.getString("color")
-			val color: Color = Color.decode(colorHexString)
 			val bukkitBiome: Biome = Biome.valueOf(biomeName)
-			biomeColors.put(color, bukkitBiome)
-			
 			try {
-				val topblock: Material = Material.valueOf(biomeSection.getString("topblock") ?: "")
-				biomeTopblocks.put(bukkitBiome, topblock)
+				val topblock: Material = Material.valueOf(biomeSection.getString("topblock") ?: "GRASS_BLOCK")
 			} catch (e: IllegalArgumentException) {
 				OwOrldGen.instance.getLogger().log(Level.SEVERE, "The topblock in the config for the biome " + bukkitBiome.toString() + " is not a material.")
 			}
+			val fineCoeff = biomeSection.getString("fine")
+			val hillCoeff = biomeSection.getString("hill")
+			val broadCoeff = biomeSection.getString("broad")
+			biomeInfos.insert(bukkitBiome, BiomeInfo(topblock, fineCoeff, hillCoeff, broadCoeff))
         }
     }
 	
 	public val seaLevel = conf.getInt("sea_level")
 	public val groundPadding = conf.getInt("ground_padding")
 	
-    private val biomeMapImagePath: String = conf.getString("biome_image")!!
-	private val heightMapImagePath: String = conf.getString("height_image")!!
-	
-    public val mapdata: Map = Map(biomeMapImagePath, heightMapImagePath, biomeColors)
-	
 	public fun getTopblock(biome: Biome): Material {
-		return biomeTopblocks.get(biome) ?: Material.GRASS_BLOCK
+		return biomeInfos.get(biome).topblock
 	}
+	
+	public fun get
 }
--- OwOrldGen/src/main/kotlin/plus/civ/oworldgen/WorldGenerator.kt
+++ OwOrldGen/src/main/kotlin/plus/civ/oworldgen/WorldGenerator.kt
@@ -11,31 +11,62 @@ import org.bukkit.generator.BlockPopulator
 import org.bukkit.util.noise.SimplexNoiseGenerator
 import java.lang.Math.*
 import kotlin.math.atan
+import org.bukkit.util.noise.SimplexNoiseGenerator
 
 class WorldGenerator(): ChunkGenerator() {
+	companion object {
+		// offsets determining where the noise is sampled from
+		private val fineOffset = 1000
+		private val hillOffset = 5000
+		private val broadOffset = 15000
+		// amplitudes (the numerator is the amplitude in blocks)
+		private val fineAmp = 2.0 / 255.0
+		private val hillAmp = 15.0 / 255.0
+		private val broadAmp = 35.0 / 255.0
+		// scales
+		private val fineScale = 0.1
+		private val hillScale = 0.5
+		private val broadScale = 2.0
+	}
+	
+	private val mapdata = OwOrldGen.configManager.mapdata
+	private val noise = SimplexNoiseGenerator.instance
 
-	val mapdata = OwOrldGen.configManager.mapdata
+	// returns an amount of blocks to shift the height at the given point
+	fun noiseAt(x: Double, z: Double, fineCoeff: Double, hillCoeff: Double, broadCoeff: Double): Double {
+		val fine = fineAmp * fineCoeff * (noise.noise(offset + x * fineScale, offset + z * fineScale) - 0.5) * 2
+		val hill = hillAmp * hillCoeff * (noise.noise(offset + x * hillScale, offset + z * hillScale) - 0.5) * 2
+		val broad = broadAmp * broadCoeff * (noise.noise(offset + x * broadScale, offset + z * broadScale) - 0.5) * 2
+		val combined = broad + hill + fine
+		return combined.clamp(-1.0, 1.0) * 255
+	}
+	
+	fun biomeAt(x: Int, z: Int): Biome {
+		return mapdata.getBiomeAt(x, z)
+	}
 
 	override fun generateChunkData(world: World, random: Random, chunkX: Int, chunkZ: Int, biomeGrid: BiomeGrid): ChunkData {
 		val chunk = createChunkData(world)
 
 		val seaLevel = OwOrldGen.configManager.seaLevel
-		// how high the layer of dirt on top of the ground is
+		// how high the layer of dirt or whatever on top of the ground is
 		val groundPadding = OwOrldGen.configManager.groundPadding
+		
 		for (x in 0..15) {
 			val realX = chunkX * 16 + x
 			for (z in 0..15) {
 				val realZ = chunkZ * 16 + z
 
-				// this may be changed to beach later sometimes, if it's ocean
-				val biome = mapdata.getBiomeAt(realX, realZ)
-				
-				// we're not generating anything in deep ocean
-				if (biome == Biome.DEEP_OCEAN) {
-					continue
+				val biome = biomeAt(realX, realZ)
+				val genericHeight = mapdata.getHeightAt(realX, realZ)
+				val heightShift = noiseAt(realX, realZ, mapdata.fineCoeffOf(biome), mapdata.hillCoeffOf(biome), mapdata.broadCoeffOf(biome))
+				val height = genericHeight + heightShift
+				if (height < seaLevel) {
+					biome = Biome.OCEAN
+				} else if (height < seaLevel + 5) {
+					biome = Biome.BEACH
 				}
 				
-				val height = mapdata.getHeightAt(realX, realZ)
 				for (y in 0..255) {
 					biomeGrid.setBiome(x, y, z, biome)
 
@@ -45,19 +76,16 @@ class WorldGenerator(): ChunkGenerator() {
 					}
 
 					if (biome == Biome.OCEAN) {
-						if (height >= seaLevel) {
-							if (y <= height && y > height - groundPadding) {
-								chunk.setBlock(x, y, z, Material.SAND)
-							} else if (y <= height) {
-								chunk.setBlock(x, y, z, Material.STONE)
-							}
-							biomeGrid.setBiome(x, y, z, Biome.BEACH)
-						} else {
-							if (y <= seaLevel && y > height) {
-								chunk.setBlock(x, y, z, Material.WATER)
-							} else if (y <= height) {
-								chunk.setBlock(x, y, z, Material.CLAY)
-							}
+						if (y <= seaLevel && y > height) {
+							chunk.setBlock(x, y, z, Material.WATER)
+						} else if (y <= height) {
+							chunk.setBlock(x, y, z, Material.CLAY)
+						}
+					} else if (biome == Biome.BEACH) {
+						if (y <= height && y > height - groundPadding) {
+							chunk.setBlock(x, y, z, Material.SAND)
+						} else if (y <= height) {
+							chunk.setBlock(x, y, z, Material.STONE)
 						}
 					} else {
 						val topMaterial = OwOrldGen.configManager.getTopblock(biome)
