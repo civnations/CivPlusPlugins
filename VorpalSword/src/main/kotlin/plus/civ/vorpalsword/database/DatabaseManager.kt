25ef06b74a603b6dc3696e0dc626d5f44f08c9ee
Ameilorate <git@amel.pw>
2020-07-27 16:05:02 -0400

    Remove AUTO_INCREMENT on id for both tables

--- VorpalSword/src/main/kotlin/plus/civ/vorpalsword/database/DatabaseManager.kt
+++ VorpalSword/src/main/kotlin/plus/civ/vorpalsword/database/DatabaseManager.kt
@@ -18,7 +18,7 @@ class DatabaseManager {
 	init {
 		val swordsTable = """
             CREATE TABLE swords (
-              id INT NOT NULL AUTO_INCREMENT,
+              id INT NOT NULL,
               world varchar(36) NOT NULL,
               x int NOT NULL,
               y int NOT NULL,
@@ -40,13 +40,13 @@ class DatabaseManager {
 
 		val prisonedPlayersTable = """
             CREATE TABLE prisoned_players (
-              id INT NOT NULL AUTO_INCREMENT,
+              id INT NOT NULL,
               player_uuid varchar(36) NOT NULL,
               killer_uuid varchar(36), 
               sword_id INT NOT NULL,
               prisoned_on bigint NOT NULL, -- unix time 
               last_seen bigint NOT NULL, -- unix time
-              
+              +  
               PRIMARY KEY (id),
               FOREIGN KEY (sword_id) REFERENCES swords(id) ON DELETE CASCADE
             )
